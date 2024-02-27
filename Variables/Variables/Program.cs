5f47cefcc9edc92775c965ac51c3bca96a741d57
SumitK27 <sumitkajbaje@gmail.com>
2024-02-27 22:21:42 +0530

    Variables

---
+++ Variables/Variables/Program.cs
@@ -0,0 +1,35 @@
+﻿namespace Variables
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            // Implicitly typed variables
+            byte number = 2;
+            int count = 10;
+            float totalPrice = 12.34f;
+            char character = 'S';
+            string firstName = "Niko";
+            bool isWorking = true;
+
+            // Explicitly typed variables
+            var anotherNumber = 3;
+
+            Console.WriteLine(number);
+            Console.WriteLine(count);
+            Console.WriteLine(totalPrice);
+            Console.WriteLine(character);
+            Console.WriteLine(firstName);
+            Console.WriteLine(isWorking);
+            Console.WriteLine(anotherNumber);
+
+            // Format strings
+            Console.WriteLine("Byte: Min {0} Max {1}", byte.MinValue, byte.MaxValue);
+            Console.WriteLine("Float: Min {0} Max {1}", float.MinValue, float.MaxValue);
+
+            // Constants
+            const float Pi = 3.14f;
+            Console.WriteLine(Pi);
+        }
+    }
+}
