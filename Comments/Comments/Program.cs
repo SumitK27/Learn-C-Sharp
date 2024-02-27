f0f8d4c8f16816c776d9d5e7da37186d1aad8d7e
SumitK27 <sumitkajbaje@gmail.com>
2024-02-27 22:22:12 +0530

    Comments

---
+++ Comments/Comments/Program.cs
@@ -0,0 +1,20 @@
+﻿namespace Comments
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            // Single line comment
+
+            /*
+             * Multi-line comment
+             */
+
+            /* Comments are ignored by the compiler. 
+             * Used to document code. 
+             * Should only be used to explain why the code is written the way it is, not what the code does.
+             */ 
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
