41f1ed1188e999693a2a4f17d6a5c91342a5e000
SumitK27 <sumitkajbaje@gmail.com>
2024-02-27 22:22:39 +0530

    Struct

---
+++ Structs/Structs/Program.cs
@@ -0,0 +1,26 @@
+﻿namespace Structs
+{
+    // A struct is a value type that is typically used to encapsulate small groups of related variables.
+    public struct Point
+    {
+        public int X;
+        public int Y;
+
+        public void Print()
+        {
+            Console.WriteLine($"X = {X}, Y = {Y}");
+        }
+    }
+
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            // Create a new Point object and assign it some values
+            Point p = new Point();
+            p.X = 10;
+            p.Y = 20;
+            p.Print();
+        }
+    }
+}
