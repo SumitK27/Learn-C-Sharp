567a2b4ff167c42a8c8a47ae7effae8e5d736b9c
SumitK27 <sumitkajbaje@gmail.com>
2024-02-27 22:22:30 +0530

    Class

---
+++ Classes/Classes/Person.cs
@@ -0,0 +1,13 @@
+﻿namespace Classes
+{
+    public class Person
+    {
+        public string firstName;
+        public string lastName;
+
+        public void Introduce()
+        {
+            Console.WriteLine("My name is " + firstName + " " + lastName);
+        }
+    }
+}
---
+++ Classes/Classes/Program.cs
@@ -0,0 +1,18 @@
+﻿using Classes.Math;
+
+namespace Classes
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Person niko = new Person();
+            niko.firstName = "Niko";
+            niko.lastName = "Bellic";
+            niko.Introduce();
+
+            var result = Calculator.Add(1, 2);
+            Console.WriteLine(result);
+        }
+    }
+}
