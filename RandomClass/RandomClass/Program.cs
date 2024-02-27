cefc184585709e4ce0db002ef99bac6a305d1a7d
SumitK27 <sumitkajbaje@gmail.com>
2024-02-28 00:50:11 +0530

    Random

---
+++ RandomClass/RandomClass/Program.cs
@@ -0,0 +1,24 @@
+﻿namespace RandomClass
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            var randomNumber = new Random();
+
+            for (int i = 0; i < 10; i++)
+            {
+                Console.WriteLine(randomNumber.Next(1, 10));
+            }
+
+            const int PassLen = 10;
+            var buffer = new char[PassLen];
+            for (int i = 0; i < PassLen; i++)
+            {
+                buffer[i] = (char)('a' + randomNumber.Next(0, 26));
+            }
+            var password = new string(buffer);
+            Console.WriteLine(password);
+        }
+    }
+}
