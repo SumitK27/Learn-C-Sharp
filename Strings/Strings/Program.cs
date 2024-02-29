3c24afd7e1d4ab9e60b1db10553a67a5ec2ba23c
SumitK27 <sumitkajbaje@gmail.com>
2024-03-01 00:57:59 +0530

    String Methods

--- Strings/Strings/Program.cs
+++ Strings/Strings/Program.cs
@@ -46,6 +46,54 @@ C:\Program Files\Microsoft";
 
             String text2 = "The quick brown fox";
             Console.WriteLine(text2.Length);
+
+            string mixedCase = "   ThE qUiCk BrOwN fOx    ";
+            Console.WriteLine("Original string: " + mixedCase);
+            Console.WriteLine("Upper Case: " + mixedCase.ToUpper());
+            Console.WriteLine("Lower Case: " + mixedCase.ToLower());
+            Console.WriteLine("Trimmed: " + mixedCase.Trim());
+            Console.WriteLine("IndexOf o: " + mixedCase.ToLower().IndexOf("o"));
+            Console.WriteLine("LastIndexOf o: " + mixedCase.ToLower().LastIndexOf("o"));
+            Console.WriteLine("Substring: " + mixedCase.Substring(4, 10));
+            Console.WriteLine("Replace: " + mixedCase.ToLower().Replace("fox", "dog"));
+
+            string[] words = mixedCase.Split(" ");
+            foreach (var word in words)
+            {
+                Console.WriteLine(word);
+            }
+
+            string empty = " ";
+            Console.WriteLine(string.IsNullOrEmpty(empty));
+            Console.WriteLine(string.IsNullOrWhiteSpace(empty));
+
+            string numberString = "123";
+            Console.WriteLine("Number string: " + numberString);
+            Console.WriteLine("Type: " + numberString.GetType());
+            
+            // May throw an exception if the string is not a number or is null
+            Console.WriteLine("Parse: " + int.Parse(numberString)); 
+            Console.WriteLine("Convert.ToInt32: " + Convert.ToInt32(numberString));
+
+            // Will not throw an exception if the string is not a number or is null
+            string emptyString = "";
+            int.TryParse(emptyString, out int parsedNumber);
+            Console.WriteLine("TryParse: " + parsedNumber);
+
+            // Number to string
+            int number = 123456;
+            Console.WriteLine("Number: " + number);
+            Console.WriteLine("ToString: " + number.ToString());
+            Console.WriteLine("ToString with format: " + number.ToString("N0"));
+            Console.WriteLine("ToString with format: " + number.ToString("N2"));
+            Console.WriteLine("ToString with format: " + number.ToString("C0"));
+
+            // Format string
+            Console.WriteLine("Formatted (Decimal): " + number.ToString("d"));
+            Console.WriteLine("Formatted (Exponential): " + number.ToString("e"));
+            Console.WriteLine("Formatted (Fixed): " + number.ToString("f"));
+            Console.WriteLine("Formatted (Hex): " + number.ToString("x"));
+
         }
     }
 }
