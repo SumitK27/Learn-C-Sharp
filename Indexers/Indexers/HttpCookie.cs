ba06623d656404fc65f1d589712a782caa734c57
SumitK27 <sumitkajbaje@gmail.com>
2024-03-02 02:07:03 +0530

    Indexer vs GetSet methods

---
+++ Indexers/Indexers/HttpCookie.cs
@@ -0,0 +1,30 @@
+﻿namespace Indexers
+{
+    public class HttpCookie
+    {
+        private readonly Dictionary<string, string> _dictionary;
+
+        public HttpCookie()
+        {
+            _dictionary = new Dictionary<string, string>();
+        }
+
+        // Indexer
+        public string this[string key]
+        {
+            get { return _dictionary[key]; }
+            set { _dictionary[key] = value; }
+        }
+
+        // GetItem and SetItem
+        public string GetItem(string key)
+        {
+            return _dictionary[key];
+        }
+
+        public void SetItem(string key, string value)
+        {
+            _dictionary[key] = value;
+        }
+    }
+}
---
+++ Indexers/Indexers/Program.cs
@@ -0,0 +1,18 @@
+﻿namespace Indexers
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            var cookie = new HttpCookie();
+
+            // Set and get the value by using the indexer
+            cookie["name"] = "Niko";
+            Console.WriteLine(cookie["name"]);
+
+            // Set and get the value by using GetItem and SetItem
+            cookie.SetItem("name", "Niko");
+            Console.WriteLine(cookie.GetItem("name"));
+        }
+    }
+}
