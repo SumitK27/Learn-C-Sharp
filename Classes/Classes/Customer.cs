7ea6af02a55f73d863be2d9b16d1400d118f38d3
SumitK27 <sumitkajbaje@gmail.com>
2024-03-02 00:32:04 +0530

    Class - Constructor, Overloading, Super

---
+++ Classes/Classes/Customer.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Classes
+{
+    public class Customer
+    {
+        public int Id;
+        public string Name;
+        public List<Order> Orders;
+
+        public Customer()
+        {
+            this.Id = 0;
+            Orders = new List<Order>();
+        }
+
+        // Overloading constructors
+        // : this() calls the default constructor
+        public Customer(string name): this()
+        {
+            this.Name = name;
+        }
+
+        // : this(name) calls the constructor with one parameter which calls the default constructor
+        // Note: This is not a good practice and makes the code harder to read and maintain
+        public Customer(int id, string name): this(name)
+        {
+            this.Id = id;
+            this.Name = name;
+        }
+    }
+}
--- Classes/Classes/Program.cs
+++ Classes/Classes/Program.cs
@@ -18,6 +18,20 @@ namespace Classes
 
             var myName = Person.Parse("Niko Bellic");
             myName.Introduce("Roman");
+
+            var customer = new Customer();
+            Console.WriteLine($"Customer ID: {customer.Id}");
+            Console.WriteLine($"Customer Name: {customer.Name}");
+
+            var customer2 = new Customer("Niko Bellic");
+            Console.WriteLine($"Customer ID: {customer2.Id}");
+            Console.WriteLine($"Customer Name: {customer2.Name}");
+
+            var customer3 = new Customer(2, "Roman Bellic");
+            var order = new Order();
+            customer3.Orders.Add(order);
+            Console.WriteLine($"Customer ID: {customer3.Id}");
+            Console.WriteLine($"Customer Name: {customer3.Name}");
         }
     }
 }
