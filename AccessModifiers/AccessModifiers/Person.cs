18afb15cb3eafba6e3aa04ffb1c54af4cbfdd127
SumitK27 <sumitkajbaje@gmail.com>
2024-03-02 01:34:19 +0530

    Access Modifier - Public, Private

---
+++ AccessModifiers/AccessModifiers/Person.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AccessModifiers
+{
+    public class Person
+    {
+        // Encapsulation
+        // private fields are not accessible outside of the class they are declared in.
+        private string _name;
+        private DateTime _birthdate;
+
+        public void SetName(string name)
+        {
+            if(!string.IsNullOrWhiteSpace(name))
+            {
+                this._name = name;
+            }
+        }
+
+        public string GetName()
+        {
+            return this._name;
+        }
+
+        public void SetBirthdate(DateTime birthdate)
+        {
+            this._birthdate = birthdate;
+        }
+
+        public DateTime GetBirthdate()
+        {
+            return this._birthdate;
+        }
+    }
+}
---
+++ AccessModifiers/AccessModifiers/Program.cs
@@ -0,0 +1,14 @@
+﻿namespace AccessModifiers
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            var person = new Person();
+            person.SetName("Niko");
+            person.SetBirthdate(new DateTime(1990, 1, 1));
+            Console.WriteLine($"Name: {person.GetName()}");
+            Console.WriteLine($"Birthdate: {person.GetBirthdate()}");
+        }
+    }
+}
