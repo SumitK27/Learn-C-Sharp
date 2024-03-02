2c04732f285eb40018d4d95b0ed681b9fe8b80a2
SumitK27 <sumitkajbaje@gmail.com>
2024-03-02 16:28:41 +0530

    UpCasting and DownCasting

---
+++ UpAndDownCasting/UpAndDownCasting/Program.cs
@@ -0,0 +1,39 @@
+﻿namespace UpAndDownCasting
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            var text = new Text();
+            // Upcasting - implicit - Converts derived class to base class
+            Shape shape = text;
+            Shape shape1 = new Text();
+
+            text.Width = 200;
+            shape.Width = 100;
+            Console.WriteLine("Text width: " + text.Width);
+
+            // StreamReader takes a Stream object as a parameter and MemoryStream is a derived class of Stream class.
+            // This is an example of upcasting
+            StreamReader reader = new StreamReader(new MemoryStream());
+
+            // Downcasting - explicit - Converts base class to derived class
+            // May throw InvalidCastException if the conversion is not possible
+            Text anotherText = (Text) shape;
+
+            // Another way to downcast
+            Text anotherText1 = shape as Text;
+            if (anotherText1 != null)
+            {
+                Console.WriteLine("Text width: " + anotherText1.Width);
+            }
+
+            // is operator
+            if (shape is Text)
+            {
+                Text anotherText2 = (Text) shape;
+                Console.WriteLine("Text width: " + anotherText2.Width);
+            }
+        }
+    }
+}
---
+++ UpAndDownCasting/UpAndDownCasting/Shape.cs
@@ -0,0 +1,15 @@
+﻿namespace UpAndDownCasting
+{
+    public class Shape
+    {
+        public int Width { get; set; }
+        public int Height { get; set; }
+        public int X { get; set; }
+        public int Y { get; set; }
+
+        public void Draw()
+        {
+            Console.WriteLine("Draw a shape");
+        }
+    }
+}
