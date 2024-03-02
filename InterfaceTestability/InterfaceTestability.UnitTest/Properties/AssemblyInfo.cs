00fb1e7b77a590d7ee6225fa406146f72b75d3e7
SumitK27 <sumitkajbaje@gmail.com>
2024-03-02 23:35:13 +0530

    Interface - Testability

---
+++ InterfaceTestability/InterfaceTestability.UnitTest/Properties/AssemblyInfo.cs
@@ -0,0 +1,20 @@
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("InterfaceTestability.UnitTest")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("InterfaceTestability.UnitTest")]
+[assembly: AssemblyCopyright("Copyright ©  2024")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+[assembly: ComVisible(false)]
+
+[assembly: Guid("9f943c1f-ef3f-4e19-baa1-1aaa95f9163d")]
+
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
---
+++ InterfaceTestability/InterfaceTestability/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace InterfaceTestability
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            /*
+             * Interface:
+             *  Used to build a loosely coupled application.
+             *  Does not contain any implementation.
+             */
+
+            var orderProcessor = new OrderProcessor(new ShippingCalculator());
+            var order = new Order { DatePlaced = DateTime.Now, TotalPrice = 100f };
+            orderProcessor.Process(order);
+        }
+    }
+}
