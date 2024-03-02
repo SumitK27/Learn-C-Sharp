9956a4a549c1badd4b9d172800afc6ba8dcdd05f
SumitK27 <sumitkajbaje@gmail.com>
2024-03-02 15:01:29 +0530

    Inheritance - Basics

---
+++ Inheritance/Inheritance/Program.cs
@@ -0,0 +1,17 @@
+﻿namespace Inheritance
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            var text = new Text();
+            text.Width = 200;
+            text.Height = 100;
+            text.FontSize = 12;
+            text.FontName = "Arial";
+            text.Copy();
+            text.Duplicate();
+            text.AddHyperlink("http://www.google.com");
+        }
+    }
+}
---
+++ Inheritance/Inheritance/Text.cs
@@ -0,0 +1,15 @@
+﻿namespace Inheritance
+{
+    // Text class inherits from PresentationObject class with is-a relationship
+    // Used for tight coupling and code reuse   
+    public class Text : PresentationObject
+    {
+        public int FontSize { get; set; }
+        public string FontName { get; set; }
+
+        public void AddHyperlink(string url)
+        {
+            Console.WriteLine("We added a link to " + url);
+        }
+    }
+}
