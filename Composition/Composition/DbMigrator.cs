e6baf84db5ff4cb878fd8a16a8bb417b82f04ef9
SumitK27 <sumitkajbaje@gmail.com>
2024-03-02 15:11:28 +0530

    Composition

---
+++ Composition/Composition/DbMigrator.cs
@@ -0,0 +1,18 @@
+﻿namespace Composition
+{
+    public class DbMigrator
+    {
+        private readonly Logger logger;
+
+        // Composition used for loose coupling
+        public DbMigrator(Logger logger)
+        {
+            this.logger = logger;
+        }
+
+        public void Migrate()
+        {
+            logger.Log("We are migrating ...");
+        }
+    }
+}
