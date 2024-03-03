113d93ee870398bffae40207353896e14d4021cd
SumitK27 <sumitkajbaje@gmail.com>
2024-03-03 16:31:56 +0530

    Dynamic

---
+++ Dynamics/Dynamics/Program.cs
@@ -0,0 +1,33 @@
+﻿namespace Dynamics
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            object obj = true;
+
+            Console.WriteLine($"GetHashCode: {obj.GetHashCode()}");
+
+            // Reflection
+            var methodInfo = obj.GetType().GetMethod("GetHashCode");
+
+            var result = methodInfo.Invoke(obj, null);
+            Console.WriteLine($"Reflection: {result}");
+
+            // Dynamic
+            dynamic dynamicObj = "Niko Bellic";
+            Console.WriteLine($"Dynamic: {dynamicObj.Replace("Niko", "Roman")}");
+
+            dynamicObj = 10;
+            Console.WriteLine($"Dynamic: {dynamicObj}");
+
+            dynamic num1 = 10;
+            dynamic num2 = 5;
+            var sum = num1 + num2; // sum is dynamic
+            int sumNum = num1 + num2; // sumNum is int
+
+            Console.WriteLine($"Dynamic: {sum}");
+            Console.WriteLine($"Dynamic: {sumNum}");
+        }
+    }
+}
