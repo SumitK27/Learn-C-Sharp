2cefa49574fd7df1490819c7c7fcdf4c56d0f6c7
SumitK27 <sumitkajbaje@gmail.com>
2024-03-03 13:10:49 +0530

    Delegate

---
+++ Delegates/Delegates/Program.cs
@@ -0,0 +1,35 @@
+﻿namespace Delegates
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            /*
+             * Delegate is a type that represents references to methods with a particular parameter list and return type.
+             * Used to pass methods as arguments to other methods as a callback.
+             * Used for making code extensible and flexible.
+             * There are few built-in delegates in C#:
+             * - Action: points to a method that takes parameters and returns void.
+             * - Func: points to a method that takes parameters and returns a value.
+             * Alternative to delegate is Interface.
+             */
+            var processor = new PhotoProcessor();
+            var filters = new PhotoFilters();
+            PhotoProcessor.PhotoFilterHandler filterHandler = filters.ApplyBrightness;
+            filterHandler += filters.ApplyContrast;
+            filterHandler += RemoveRedEye;
+            processor.Process("image.png", filterHandler);
+
+            var processor2 = new PhotoProcessor();
+            Action<Photo> filterHandler2 = filters.ApplyBrightness;
+            filterHandler2 += filters.ApplyContrast;
+            filterHandler2 += RemoveRedEye;
+            processor2.Process("image.png", filterHandler2);
+        }
+
+        static void RemoveRedEye(Photo photo)
+        {
+            Console.WriteLine("Removing red eye...");
+        }
+    }
+}
